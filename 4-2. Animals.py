b9dc1baffdb32e44ac2efe882b3a4e2cbc4103a2
CalebHensley <157909870+CalebHensley@users.noreply.github.com>
2024-02-11 13:22:00 -0500

    Add files via upload

---
+++ 4-2. Animals.py
@@ -0,0 +1,17 @@
+animals = ['tiger', 'lion', 'cheetah']
+
+tiger = "Tigers can use their ears to communicate."
+lion = "Lions are the only cats who roar together."
+cheetah = "Cheetahs can spot prey up to three miles away."
+
+statements = [tiger, lion, cheetah]
+
+common = "These three animals belong to the Felidae family, and are territorial in nature. They mark their territories with scent to deter intruders and find mates."
+
+for animal in animals:
+  print(animal)
+
+for statement in statements:
+  print(statement)
+
+print(common)
